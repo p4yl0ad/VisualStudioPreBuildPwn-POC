3d46aade3d93acd650d5374d408eb667cfe757c2
p4yl0ad <57832389+p4yl0ad@users.noreply.github.com>
2021-01-26 13:28:50 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace preBexecPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
